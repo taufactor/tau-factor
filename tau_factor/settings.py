15862040fb07ef4db61c91f33fd0d144802ceef6
Tau-Factor <factor.tau@gmail.com>
2021-06-15 00:54:36 +0300

    TAU-9 - Remove DATABASES from configuration as Heroku manages this

--- tau_factor/settings.py
+++ tau_factor/settings.py
@@ -97,17 +97,6 @@ TEMPLATES = (
 
 WSGI_APPLICATION = "tau_factor.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
