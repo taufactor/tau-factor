d7883156bd9502742e0ad5c798fa1b2c38c7ff60
Tau-Factor <factor.tau@gmail.com>
2021-09-17 23:35:57 +0300

    TAU-39 - Course Instance - Serialize fial statistics

--- courses/serializers/downstream_serializers.py
+++ courses/serializers/downstream_serializers.py
@@ -1,7 +1,14 @@
-from rest_framework import fields
+import typing
 
+from drf_yasg.utils import swagger_serializer_method
+from rest_framework import fields
+from rest_framework import serializers
+
+from courses import defines as courses_defines
 from courses import models as courses_models
 from courses.serializers import common_serializers as courses_common_serializers
+from grades import models as grades_models
+from grades import serializers as grades_serializers
 
 
 class CourseGroupDownstreamSerializer(courses_common_serializers.BaseCourseGroupSerializer):
@@ -16,11 +23,36 @@ class CourseGroupDownstreamSerializer(courses_common_serializers.BaseCourseGroup
 class CourseInstanceDownstreamSerializer(courses_common_serializers.BaseCourseInstanceSerializer):
     groups = CourseGroupDownstreamSerializer(many=True)
     num_groups = fields.IntegerField()
+    statistics = serializers.SerializerMethodField()
+
+    @swagger_serializer_method(grades_serializers.ExamStatisticsSerializer(allow_null=True))
+    def get_statistics(
+            self,
+            instance: courses_models.CourseInstance,
+    ) -> typing.Optional[typing.Dict[str, typing.Any]]:
+        statistics = grades_models.ExamStatistics.objects.filter(**{
+            "__".join((
+                grades_models.ExamStatistics.exam.field.name,
+                grades_models.Exam.course_group.field.name,
+                courses_models.CourseGroup.course_instance.field.name,
+            )): instance,
+            "__".join((
+                grades_models.ExamStatistics.exam.field.name,
+                grades_models.Exam.course_group.field.name,
+                courses_models.CourseGroup.course_group_name.field.name,
+            )): courses_defines.COURSE_GROUP_ALL_NAME,
+            "__".join((
+                grades_models.ExamStatistics.exam.field.name,
+                grades_models.Exam.moed.field.name,
+            )): 0,  # final grades
+        }).first()
+        return grades_serializers.ExamStatisticsSerializer(instance=statistics).data if statistics else None
 
     class Meta(courses_common_serializers.BaseCourseInstanceSerializer.Meta):
         fields = courses_common_serializers.BaseCourseInstanceSerializer.Meta.fields + (
             courses_models.CourseInstance.COURSE_GROUPS_RELATED_FIELD_NAME,
             courses_models.CourseInstance.NUM_GROUPS_ANNOTATION,
+            "statistics",
         )
 
 
